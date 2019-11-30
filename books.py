8cb0f040b448dc60b496a8a17f7cf776140f99ad
Frank Hui <huifrank@live.com>
2019-11-30 15:07:22 -0800

    Turn to json

--- books.py
+++ books.py
@@ -8,8 +8,8 @@ for i in range(0,100000):
         book = {}
         book["text"] = text;
         book_json = json.dumps(book)
-        with open("books/" + str(i) + '.txt', 'w', encoding='utf-8') as outfile:
+        with open("books/" + str(i) + '.json', 'w', encoding='utf-8') as outfile:
             print(book_json, file=outfile)
-        print("Saved " + str(i) + " to books/" + str(i) + ".txt")
+        print("Saved " + str(i) + " to books/" + str(i) + ".json")
     except:
         print(str(i) + " does not exist.")
