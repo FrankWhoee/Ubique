e8268ef93cccfec284189d05f9ffb578aba8d17a
Frank Hui <huifrank@live.com>
2019-11-30 15:03:09 -0800

    Add script for downloading booksd

---
+++ books.py
@@ -0,0 +1,15 @@
+from gutenberg.acquire import load_etext
+from gutenberg.cleanup import strip_headers
+import json
+
+for i in range(0,100000):
+    try:
+        text = strip_headers(load_etext(i)).strip()
+        book = {}
+        book["text"] = text;
+        book_json = json.dumps(book)
+        with open("books/" + str(i) + '.txt', 'w', encoding='utf-8') as outfile:
+            print(book_json, file=outfile)
+        print("Saved " + str(i) + " to books/" + str(i) + ".txt")
+    except:
+        print(str(i) + " does not exist.")
