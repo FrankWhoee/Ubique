ed1900b382eb7945767ec406ee40dc0b823cb8a0
Aszerious <matthewhhuang@gmail.com>
2019-11-30 16:29:10 -0800

    Update server

--- server.py
+++ server.py
@@ -61,18 +61,6 @@ def book_get():
 @app.route('/<article>')
 def main(article):
     zimply_article_request = requests.get('http://localhost:9454/{}'.format(article))
-
-    soup = BeautifulSoup(zimply_article_request.content)
-
-    for image in soup.find_all(['image', 'img']):
-        image.decompose()
-
-    for extra_tags in soup.find_all(['b', 'a', 'span']):
-        extra_tags.unwrap()
-
-    title_e = soup.find('h1', { 'class': 'section-heading' }).text.strip()
-    body_e = soup.find('div', { 'id': 'mf-section-0' }).text.strip()
-
-    return render_template("wiki.html", title = title_e, content = body_e)
+    return zimply_article_request.content
 
     #return json.dumps({ 'title': title_e.text.strip(), 'body': body_e.text.strip() })
